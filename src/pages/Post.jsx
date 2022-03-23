fa481b6514afc016a39cc7f98abed12a17db1534
pierluigi <pierluigibaiano@gmail.com>
2022-03-23 14:44:55 +0100

    use useLocation to find a post

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { useLocation } from "react-router";
 import { posts } from "../data";
 
 const Post = () => {
-  const post = posts[2];
+  // const post = posts[2];
+
+  const location = useLocation();
+  console.log(location);
+
+  const path = location.pathname.split("/")[2];
+  console.log(path);
+
+  const post = posts.find((p) => p.id.toString() === path);
+  console.log(post);
+
   return (
     <div className="post">
       <img src={post.img} alt="" className="postImg" />
