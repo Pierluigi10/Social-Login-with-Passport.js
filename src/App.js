6f0e47e1af1706f2fbf8d2ef475c157275319fb0
pierluigi <pierluigibaiano@gmail.com>
2022-03-23 12:00:30 +0100

    copy dummy data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import "./app.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
