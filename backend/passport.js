a09614da12b7f1d2ddd2196e43302cf233db176b
pierluigi <pierluigibaiano@gmail.com>
2022-03-23 16:29:39 +0100

    clean code

--- backend/passport.js
+++ backend/passport.js
@@ -3,11 +3,8 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 // const FacebookStrategy = require("passport-facebook").Strategy;
 const passport = require("passport");
 
-// const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-// const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-const GOOGLE_CLIENT_ID =
-  "620948409363-ec9gubg5kt40rh12vhgtl3etiesscat2.apps.googleusercontent.com";
-const GOOGLE_CLIENT_SECRET = "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";
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 
 passport.use(
   new GoogleStrategy(
